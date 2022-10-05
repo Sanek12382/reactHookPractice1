15e0826606a73610bc0dc45b00e5700b63156c28
Sanek12382 <58268711+Sanek12382@users.noreply.github.com>
2022-10-05 15:24:10 +0300

    Update counter.js

--- src/comp/counter.js
+++ src/comp/counter.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
  export function CounterComp ( {count, text}  ){
-    if( text[text.length]!=" ")
-    {text=text+" ";}
-    if (count % 2===0){
-        text=text+"even "
-    } else { text=text+"odd "}
+   text=text+" ";  
     return(
        <p>{text}{count}</p>
     )

d2a606f15ff9205a4227ea8d25b1d408ccfbade1
Sanek12382 <aleksondrv@bk.ru>
2022-10-05 15:17:19 +0300

    first commit

--- src/comp/counter.js
+++ src/comp/counter.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
  export function CounterComp ( {count, text}  ){
-    text=text+" ";
+    if( text[text.length]!=" ")
+    {text=text+" ";}
+    if (count % 2===0){
+        text=text+"even "
+    } else { text=text+"odd "}
     return(
        <p>{text}{count}</p>
     )
